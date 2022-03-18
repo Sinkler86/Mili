ecb43adf28e7cf340af85c8bdbfd8fe273e038ca
Jhamyl Tukhvatullin <sinkler86@gmail.com>
2022-03-18 15:53:13 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 2750;
+        int MilyaVRub = 20;
+        int bonus = ticketPrice / MilyaVRub;
+        System.out.println("Получено миль: " + bonus);
+
+    }
+}
